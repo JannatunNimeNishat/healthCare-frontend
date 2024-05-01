7afcf51e49198ad16028c94c66ff4fecc8e58fa4
JannatunNimeNishat <nishatnime100@gmail.com>
2024-05-01 20:52:27 +0600

    project starter with mui, tailwindCss, nextjs 14.0.3

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material';
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <Button variant="text">Text</Button>
+    </div>
+  );
+};
+
+export default HomePage;
