6e97ea43c43e2bc46acf1d5bcba4611bc44e868c
JannatunNimeNishat <nishatnime100@gmail.com>
2024-05-02 22:31:31 +0600

    Navbar added

---
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Stack py={2} direction={"row"} alignItems={"center"} justifyContent={"space-between"}>
+        <Typography component={Link} href="/"  variant="h4"  fontWeight={600}>
+          P<Box component={"span"} color={"primary.main"}>H</Box> Health Care
+        </Typography>
+        {/* menu options */}
+      <Stack direction={"row"} gap={4} justifyContent={"space-between"}>
+        <Typography component={Link} href="/consultation">Consultation</Typography>
+        <Typography >Health Plans</Typography>
+        <Typography>Medicine</Typography>
+        <Typography>Diagnostics</Typography>
+        <Typography>NGOs</Typography>
+      </Stack>
+      {/* button */}
+      <Button component={Link} href="/login">Login</Button>
+      </Stack>
+    </Container>
+  );
+};
+
+export default Navbar;
