8d0139f4c25faa8bee21a9a79f2a1d625a4deebd
JannatunNimeNishat <nishatnime100@gmail.com>
2024-05-09 01:27:12 +0600

    isLoggesIn and removeUser functions added

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,27 +1,44 @@
-"use client"
-import { getUserInfo } from "@/services/actions/auth.sevices";
+"use client";
+import { getUserInfo, isLoggedIn } from "@/services/actions/auth.sevices";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import Link from "next/link";
 
 const Navbar = () => {
   const userInfo = getUserInfo();
-  console.log(userInfo);
+ 
   return (
     <Container>
-      <Stack py={2} direction={"row"} alignItems={"center"} justifyContent={"space-between"}>
-        <Typography component={Link} href="/"  variant="h4"  fontWeight={600}>
-          P<Box component={"span"} color={"primary.main"}>H</Box> Health Care
+      <Stack
+        py={2}
+        direction={"row"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+      >
+        <Typography component={Link} href="/" variant="h4" fontWeight={600}>
+          P
+          <Box component={"span"} color={"primary.main"}>
+            H
+          </Box>{" "}
+          Health Care
         </Typography>
         {/* menu options */}
-      <Stack direction={"row"} gap={4} justifyContent={"space-between"}>
-        <Typography component={Link} href="/consultation">Consultation</Typography>
-        <Typography >Health Plans</Typography>
-        <Typography>Medicine</Typography>
-        <Typography>Diagnostics</Typography>
-        <Typography>NGOs</Typography>
-      </Stack>
-      {/* button */}
-      <Button component={Link} href="/login">Login</Button>
+        <Stack direction={"row"} gap={4} justifyContent={"space-between"}>
+          <Typography component={Link} href="/consultation">
+            Consultation
+          </Typography>
+          <Typography>Health Plans</Typography>
+          <Typography>Medicine</Typography>
+          <Typography>Diagnostics</Typography>
+          <Typography>NGOs</Typography>
+        </Stack>
+        {/* button */}
+        {userInfo?.userId ? (
+          <Button color="error">Logout</Button>
+        ) : (
+          <Button component={Link} href="/login">
+            Login
+          </Button>
+        )}
       </Stack>
     </Container>
   );
--- src/services/actions/auth.sevices.ts
+++ src/services/actions/auth.sevices.ts
@@ -1,18 +1,32 @@
-import { authKey } from "@/constants/authkey"
-import { decodedToken } from "@/utils/jwt"
-import { getFromLocalStorage, setToLocalStorage } from "@/utils/local-storage"
+import { authKey } from "@/constants/authkey";
+import { decodedToken } from "@/utils/jwt";
+import {
+  getFromLocalStorage,
+  removeFromLocalStorage,
+  setToLocalStorage,
+} from "@/utils/local-storage";
 
-export const storeUserInfo = ({accessToken}:{accessToken:string})=>{
-    return setToLocalStorage(authKey,accessToken)
-}
+export const storeUserInfo = ({ accessToken }: { accessToken: string }) => {
+  return setToLocalStorage(authKey, accessToken);
+};
 
-export const getUserInfo = ()=>{
-    const authToken = getFromLocalStorage(authKey);
-    if(authToken){
-        const decodedData:any = decodedToken(authToken);
-        return {
-            ...decodedData,
-            role:decodedData?.role?.toLowerCase()
-        }
-    }
-}
+export const getUserInfo = () => {
+  const authToken = getFromLocalStorage(authKey);
+  if (authToken) {
+    const decodedData: any = decodedToken(authToken);
+    return {
+      ...decodedData,
+      role: decodedData?.role?.toLowerCase(),
+    };
+  }
+};
+
+export const isLoggedIn = () => {
+  const authToken = getFromLocalStorage(authKey);
+  if (authToken) {
+    return !!authToken;
+  }
+};
+export const removeUser = () => {
+  return removeFromLocalStorage(authKey);
+};
