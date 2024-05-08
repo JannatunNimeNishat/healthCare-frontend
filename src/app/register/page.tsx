3d29c19a8b6e7e22fdd368e652ad885be2d65c24
JannatunNimeNishat <nishatnime100@gmail.com>
2024-05-08 23:46:08 +0600

    react hook form added

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,14 +1,46 @@
-import { Box, Button, Container, Grid, Stack, TextField, Typography } from "@mui/material";
+"use client";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import assets from "@/assets";
 import Image from "next/image";
 import Link from "next/link";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+
+
+interface IPatientData {
+name: string;
+  email: string;
+  contactNumber: string;
+  address: string;
+}
+
+interface IPatientRegisterFromData {
+  password: string;
+  patient: IPatientData
+}
 
 const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<IPatientRegisterFromData>();
+  const onSubmit: SubmitHandler<IPatientRegisterFromData> = (data) => console.log(data);
+
   return (
-    <Container >
+    <Container>
       <Stack
         sx={{
-            height:"100vh",
+          height: "100vh",
           justifyContent: "center",
           alignItems: "center",
         }}
@@ -20,10 +52,10 @@ const RegisterPage = () => {
             boxShadow: 1,
             borderRadius: 1,
             p: 4,
-            textAlign:"center",
+            textAlign: "center",
           }}
         >
-            {/* logo */}
+          {/* logo */}
           <Stack
             sx={{
               justifyContent: "center",
@@ -31,72 +63,86 @@ const RegisterPage = () => {
             }}
           >
             <Box>
-                <Image src={assets.svgs.logo} width={50} height={50} alt="logo"/>
+              <Image src={assets.svgs.logo} width={50} height={50} alt="logo" />
             </Box>
             <Box>
-                <Typography variant="h6" fontWeight={600}>Patient Register</Typography>
+              <Typography variant="h6" fontWeight={600}>
+                Patient Register
+              </Typography>
             </Box>
           </Stack>
           {/* form */}
           <Box>
-          <form>
-          <Grid container spacing={3} my={1}>
-            {/* 1st row */}
-            <Grid item md={12}>
-            <TextField  
-            label="Name"
-             variant="outlined"
-             size="small"
-             fullWidth={true}
-             />
-            </Grid>
-            {/* 2nd row1 */}
-            <Grid item md={6}>
-            <TextField  
-            type="email"
-            label="Email"
-             variant="outlined"
-             size="small"
-             fullWidth={true}
-             />
-            </Grid>
-            {/* 2nd row2 */}
-            <Grid item md={6}>
-            <TextField  
-            type="password"
-            label="Password"
-             variant="outlined"
-             size="small"
-             fullWidth={true}
-             />
-            </Grid>
-            {/* 3rd row1 */}
-            <Grid item md={6}>
-            <TextField  
-            type="tel"
-            label="Contact Number"
-             variant="outlined"
-             size="small"
-             fullWidth={true}
-             />
-            </Grid>
-            {/* 3rd row2 */}
-            <Grid item md={6}>
-            <TextField  
-            type="text"
-            label="Address"
-             variant="outlined"
-             size="small"
-             fullWidth={true}
-             />
-            </Grid>
-          </Grid>
-          {/* buttons */}
-          <Button sx={{margin:"10px 10px"}} fullWidth={true}>Register</Button>
-          <Typography component="p" fontWeight={300}>Do you already have an account? 
-          <Link href={'/login'}> Login</Link>
-          </Typography>
-          </form>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Grid container spacing={3} my={1}>
+                {/* 1st row */}
+                <Grid item md={12}>
+                  <TextField
+                    label="Name"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    {...register("patient.name")}
+                  />
+                </Grid>
+                {/* 2nd row1 */}
+                <Grid item md={6}>
+                  <TextField
+                    type="email"
+                    label="Email"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    {...register("patient.email")}
+                  />
+                </Grid>
+                {/* 2nd row2 */}
+                <Grid item md={6}>
+                  <TextField
+                    type="password"
+                    label="Password"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    {...register("password")}
+                  />
+                </Grid>
+                {/* 3rd row1 */}
+                <Grid item md={6}>
+                  <TextField
+                    type="tel"
+                    label="Contact Number"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    {...register("patient.contactNumber")}
+                  />
+                </Grid>
+                {/* 3rd row2 */}
+                <Grid item md={6}>
+                  <TextField
+                    type="text"
+                    label="Address"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    {...register("patient.address")}
+                  />
+                </Grid>
+              </Grid>
+              {/* buttons */}
+              <Button
+                type="submit"
+                sx={{ margin: "10px 10px" }}
+                fullWidth={true}
+              >
+                Register
+              </Button>
+              <Typography component="p" fontWeight={300}>
+                Do you already have an account?
+                <Link href={"/login"}> Login</Link>
+              </Typography>
+            </form>
           </Box>
         </Box>
       </Stack>
