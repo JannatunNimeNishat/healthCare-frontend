1ea24da8339a90de173afc18c8ccd00022dee593
JannatunNimeNishat <nishatnime100@gmail.com>
2024-05-17 00:43:17 +0600

    SpecialtiesModal and PHFileUploader added

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+"use client"
+import { Box, Button, Stack, TextField } from "@mui/material";
+import SpecialtiesModal from "./components/SpecialtiesModal";
+import { useState } from "react";
 
 const SpecialtiesPage = () => {
-    return (
-        <div>
-            <h1>specialties page</h1>
-        </div>
-    );
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  return (
+    <Box>
+      <Stack
+        direction={"row"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
+        <Button
+        onClick={()=>setIsModalOpen(true)}
+        >Create Specialty</Button>
+        <SpecialtiesModal 
+        open={isModalOpen}
+        setOpen={setIsModalOpen}
+        />
+        <TextField size="small" placeholder="Search Specialties" />
+      </Stack>
+    </Box>
+  );
 };
 
 export default SpecialtiesPage;
