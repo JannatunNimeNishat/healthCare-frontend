d9807124c03aab93ef090ca5b0dc529b4a4d2554
JannatunNimeNishat <nishatnime100@gmail.com>
2024-05-17 01:44:20 +0600

    updated

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,27 +1,29 @@
-"use client"
+"use client";
 import { Box, Button, Stack, TextField } from "@mui/material";
 
 import { useState } from "react";
-import SpecialtyModal from "./components/SpecialtiesModal";
+import SpecialtyModal from "./components/SpecialtyModal";
+import { useGetAllSpecialtiesQuery } from "@/redux/api/specialtiesApi";
 
 const SpecialtiesPage = () => {
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const { data, isLoading } = useGetAllSpecialtiesQuery({});
   return (
     <Box>
+      {/* nav */}
       <Stack
         direction={"row"}
         justifyContent={"space-between"}
         alignItems={"center"}
       >
-        <Button
-        onClick={()=>setIsModalOpen(true)}
-        >Create Specialty</Button>
-        <SpecialtyModal 
-        open={isModalOpen}
-        setOpen={setIsModalOpen}
-        />
+        <Button onClick={() => setIsModalOpen(true)}>Create Specialty</Button>
+        <SpecialtyModal open={isModalOpen} setOpen={setIsModalOpen} />
         <TextField size="small" placeholder="Search Specialties" />
       </Stack>
+      {/* table */}
+      <Box>
+        <h1>Display Specialties</h1>
+      </Box>
     </Box>
   );
 };
