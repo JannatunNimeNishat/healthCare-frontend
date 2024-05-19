7456d9dca1802b70d0784c5b85154b024d72b8d9
JannatunNimeNishat <nishatnime100@gmail.com>
2024-05-19 17:53:34 +0600

    fullScreen modal added

--- src/app/(withDashboardLayout)/dashboard/admin/doctors/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/doctors/page.tsx
@@ -1,19 +1,23 @@
+"use client"
 import { Box, Button, Stack, TextField } from "@mui/material";
-
+import DoctorModal from "./components/DoctorModal";
+import { useState } from "react";
 
 const DoctorsPage = () => {
-    return (
-        <Box>
-            <Stack
-            direction={"row"}
-            justifyContent={"space-between"}
-            alignItems={"center"}
-            >
-                <Button>CREATE NEW DOCTOR</Button>
-                <TextField size="small" placeholder="Search doctors" />
-            </Stack>
-        </Box>
-    );
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  return (
+    <Box>
+      <Stack
+        direction={"row"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
+        <Button onClick={() => setIsModalOpen(true)}>Create New Doctor</Button>
+        <DoctorModal open={isModalOpen} setOpen={setIsModalOpen} />
+        <TextField size="small" placeholder="Search doctors" />
+      </Stack>
+    </Box>
+  );
 };
 
 export default DoctorsPage;
