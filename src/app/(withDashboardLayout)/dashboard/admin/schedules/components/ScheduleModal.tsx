ad2862818d966e5945c93ae419359a65c630d20d
JannatunNimeNishat <nishatnime100@gmail.com>
2024-06-07 21:57:59 +0600

    Updated

--- src/app/(withDashboardLayout)/dashboard/admin/schedules/components/ScheduleModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/schedules/components/ScheduleModal.tsx
@@ -10,32 +10,30 @@ import { FieldValues } from "react-hook-form";
 import { toast } from "sonner";
 
 type TProps = {
-    open: boolean;
-    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  };
-const ScheduleModal = ({ open, setOpen }: TProps)=> {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+const ScheduleModal = ({ open, setOpen }: TProps) => {
   const [createSchedule] = useCreateScheduleMutation();
-    const handleFormSubmit = async (values: FieldValues) => {
-         console.log(values);
-         values.startDate = dateFormatter(values.startDate);
-        values.endDate = dateFormatter(values.endDate);
-        values.startTime = timeFormatter(values.startTime);
-        values.endTime = timeFormatter(values.endTime); 
-        // console.log(values);
-        try {
-          const res = await createSchedule(values).unwrap();
-          // console.log(res);
-          if (res?.length) {
-            toast.success("Schedules created successfully!");
-            setOpen(false);
-          }
-        } catch (err: any) {
-        //   console.error(err.message);
-        }
-      };
+  const handleFormSubmit = async (values: FieldValues) => {
+    console.log(values);
+    values.startDate = dateFormatter(values.startDate);
+    values.endDate = dateFormatter(values.endDate);
+    values.startTime = timeFormatter(values.startTime);
+    values.endTime = timeFormatter(values.endTime);
 
-    return (
-        <PHModal open={open} setOpen={setOpen} title="Create Schedule">
+    try {
+      const res = await createSchedule(values).unwrap();
+
+      if (res?.length) {
+        toast.success("Schedules created successfully!");
+        setOpen(false);
+      }
+    } catch (err: any) {}
+  };
+
+  return (
+    <PHModal open={open} setOpen={setOpen} title="Create Schedule">
       <PHForm onSubmit={handleFormSubmit}>
         <Grid container spacing={2} sx={{ width: "400px" }}>
           <Grid item md={12}>
@@ -56,7 +54,7 @@ const ScheduleModal = ({ open, setOpen }: TProps)=> {
         </Button>
       </PHForm>
     </PHModal>
-    );
+  );
 };
 
 export default ScheduleModal;
